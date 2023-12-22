655bff90393f7f561abd3641daed5434d8d4214f
hmsmiraz <hmsmiraz64729@gmail.com>
2023-12-22 23:15:01 +0600

    initial setup

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <div>
+             <h2 className="text-center text-2xl font-bold">Hello world</h2>
+        </div>
+    );
+};
+
+export default Home;
